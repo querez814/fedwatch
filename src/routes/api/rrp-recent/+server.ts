6958bb673e44c6f5edfe02e62f153b62054f62c6
Zaki Quereshy <zakiquereshy@Zakis-MacBook-Pro-2.local>
2025-09-20 02:24:32 -0400

    backend

---
+++ src/routes/api/rrp-recent/+server.ts
@@ -0,0 +1,41 @@
+import { VITE_FRED_API } from '$env/static/private';
+import type { RequestHandler } from './$types';
+import {json} from '@sveltejs/kit'
+import * as dfd from 'danfojs'
+import.meta.env.VITE_FRED_API
+
+
+export const GET: RequestHandler = async () => {
+
+    if (!VITE_FRED_API){
+        return new Response('Fred API KEY NOT WORKING OR SET', {status: 500})
+    }
+    const today = new Date().toISOString().split('T')[0]; 
+    const thirtyDaysAgo = new Date(Date.now() - 60 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+    const url = `https://api.stlouisfed.org/fred/series/observations?series_id=RRPONTSYD&api_key=${VITE_FRED_API}&file_type=json&observation_start=${thirtyDaysAgo}&observation_end=${today}`;
+    const res = await fetch(url)
+    if(!res.ok){
+        return new Response('Failed to fetch RRPONTSYD', {status: 500})
+    }
+    const data = await res.json()
+    const obs = data.observations
+    const df = new dfd.DataFrame(obs)
+    const dfFilter = df.loc({columns: ['date', 'value']})
+    dfFilter.addColumn("value", dfFilter["value"].values.map(parseFloat), { inplace: true });
+    
+    const values = dfFilter["value"].values;
+    const pctChange = values.map((current: any, index: any) => {
+        if (index === 0) return null; 
+        const previous = values[index - 1];
+        if (previous !== 0) {
+            const change = ((current - previous) / previous) * 100;
+            return parseFloat(change.toFixed(2));
+        }
+        return null;
+    });
+    
+    dfFilter.addColumn("pct_change", pctChange, { inplace: true });
+    
+    const output = dfd.toJSON(dfFilter)
+    return json({output})
+};
